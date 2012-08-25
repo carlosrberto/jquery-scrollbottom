34f6652ac39d5c80e99bf9dc1bfb3992e6b4e099
Carlos Roberto <carlos.rberto@gmail.com>
2012-08-25 12:42:03 -0300

    plugin code

---
+++ scrollbottom.js
@@ -0,0 +1,51 @@
+/*
+* jQuery scrollBottom
+* http://carlosroberto.name/
+*
+* Copyright (c) 2012 Carlos Roberto Gomes Junior
+* Licensed under a Creative Commons Attribution 3.0 License
+* http://creativecommons.org/licenses/by-sa/3.0/
+*
+* Version: 0.1
+*/
+
+(function($) {
+    var defaults = {
+        offsetY : 0,
+        callback: function() {}
+    }
+    
+    function ScrollBottom (win, options) {
+
+        // plugin options
+        if ( options ) {
+            // extend options
+            this.options = $.extend(defaults, options); 
+        } else {
+            // use default options
+            this.options = defaults;
+        };
+
+        this.scrollTop = $(document).scrollTop();
+        this.pageHeight = $(document).height() - $(window).height();
+        if ( win === window ) {
+            $(win).bind('scroll', $.proxy(this.scrollHandler, this));
+        }
+
+    }
+
+    ScrollBottom.prototype.scrollHandler = function(event) {
+        var direction = $(document).scrollTop() > this.scrollTop ? 'bottom' : 'top';
+        this.scrollTop = $(document).scrollTop();
+        this.pageHeight = $(document).height() - $(window).height();
+        if ( this.scrollTop >= this.pageHeight+this.options.offsetY && typeof this.options.callback === 'function' ) {
+            this.options.callback.apply(this, [event, {'scrollTop':this.scrollTop, 'pageHeight':this.pageHeight, 'direction' : direction}]);
+        };
+    }
+
+    $.fn.scrollBottom = function(options) {
+        this.each(function() {
+            new ScrollBottom(this, options);
+        });
+    };
+})(jQuery)

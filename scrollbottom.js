04cbc0ba0ffd84d2d20930273fcf625e95a7ee36
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2012-08-30 09:17:41 -0300

    better handling of scroll event

--- scrollbottom.js
+++ scrollbottom.js
@@ -1,19 +1,30 @@
 /*
 * jQuery scrollBottom
-* http://carlosroberto.name/
+* https://github.com/carlosrberto/jquery-scrollbottom
 *
 * Copyright (c) 2012 Carlos Roberto Gomes Junior
 * Licensed under a Creative Commons Attribution 3.0 License
 * http://creativecommons.org/licenses/by-sa/3.0/
 *
-* Version: 0.1
+* Version: 0.1.1
 */
+
+// simple scrollend Event
+$(function(){
+    var scrollTimer;
+    $(window).on('scroll', function() {
+        clearTimeout( scrollTimer );
+        scrollTimer = setTimeout( function(){
+            $(window).trigger('scrollBottom.scrollend');
+        }, 200);
+    });          
+});
 
 (function($) {
     var defaults = {
         offsetY : 0,
         callback: function() {}
-    }
+    };
     
     function ScrollBottom (win, options) {
 
@@ -24,12 +35,12 @@
         } else {
             // use default options
             this.options = defaults;
-        };
+        }
 
         this.scrollTop = $(document).scrollTop();
         this.pageHeight = $(document).height() - $(window).height();
         if ( win === window ) {
-            $(win).bind('scroll', $.proxy(this.scrollHandler, this));
+            $(win).on('scrollBottom.scrollend', $.proxy(this.scrollHandler, this));
         }
 
     }
@@ -40,12 +51,12 @@
         this.pageHeight = $(document).height() - $(window).height();
         if ( this.scrollTop >= this.pageHeight+this.options.offsetY && typeof this.options.callback === 'function' ) {
             this.options.callback.apply(this, [event, {'scrollTop':this.scrollTop, 'pageHeight':this.pageHeight, 'direction' : direction}]);
-        };
-    }
+        }
+    };
 
     $.fn.scrollBottom = function(options) {
         this.each(function() {
             new ScrollBottom(this, options);
         });
     };
-})(jQuery)
+})(jQuery);
